63d55c7896e67d59ccd91deaff37b64df1ec0ca6
Nishchal49 <145098428+Nishchal49@users.noreply.github.com>
2024-08-26 20:25:57 +0530

    JS Files made

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Nav from "./components/Nav"
+import Header from "./components/Routing"
+import Main from "./components/Main"
+import Footer from "./components/Footer"
 
 function App() {
   return (
-    <div className="App">
-      Homepage
+    <div>
+    <React.Fragment>
+      <Nav />
+      <Main />
+      <Footer />
+    </React.Fragment>
     </div>
   );
 }
